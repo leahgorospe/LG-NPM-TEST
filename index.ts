b5304305bfe23dc48695750e1753294927b3e494
Leah Gorospe <Leah.Gorospe@BizData.com.au>
2022-02-25 15:54:54 +1100

    test

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { NotificationsHubConnection2 } from "./signalr/NotificationsHubConnection2";
-import { BaseMonitorHubConnection2 } from "./signalr/BaseMonitorHubConnection2";
 
 function registerNotification(signalRBase, token) {
+    var NotificationsHubConnection2 = require('./signalr/NotificationsHubConnection2');
+
     alert('Initialise ' + signalRBase);
     console.log("Initialize StoredNotificationHubConnection");
     NotificationsHubConnection2.init(signalRBase, token);
