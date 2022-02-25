3ee2468be6489ceaec0d90023a624e7f6f97cfa6
Leah Gorospe <Leah.Gorospe@BizData.com.au>
2022-02-25 15:20:38 +1100

    asdad

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import { NotificationsHubConnection2 } from "./signalr/NotificationsHubConnection2";
+import { BaseMonitorHubConnection2 } from "./signalr/BaseMonitorHubConnection2";
 
-function registerNotification(signalRBase, token) {
-    alert('Initialise ' + signalRBase);
-    console.log("Initialize StoredNotificationHubConnection");
-    NotificationsHubConnection2.init(signalRBase, token);
-}
+////function registerNotification(signalRBase, token) {
+////    alert('Initialise ' + signalRBase);
+////    console.log("Initialize StoredNotificationHubConnection");
+////    NotificationsHubConnection2.init(signalRBase, token);
+////}
 
+
+module.exports = {
+    NotificationsHubConnection2,
+    BaseMonitorHubConnection2
+};
+
+//https://stackoverflow.com/questions/32657516/how-to-properly-export-an-es6-class-in-node-4
