159d10ddf82fdf694f65123041754f1316ff1d4c
Leah Gorospe <Leah.Gorospe@BizData.com.au>
2022-02-28 13:50:32 +1100

    sadasd

--- signalr/NotificationsHubConnection2.ts
+++ signalr/NotificationsHubConnection2.ts
@@ -1,9 +1,4 @@
-////import { BaseMonitorHubConnection2 } from "./BaseMonitorHubConnection2";
-//import  * as sendNotification2  from "./SignalRConstants";
-/*import  StoredNotification2  from "./../interfaces/StoredNotification2";*/
-const { sendNotification2 } = require('./SignalRConstants');
 const { BaseMonitorHubConnection2 } = require ("./BaseMonitorHubConnection2");
-/*const StoredNotification2 = require("./../interfaces/StoredNotification2");*/
 
 interface StoredNotification {
     id: number;
@@ -19,13 +14,10 @@ export class NotificationsHubConnection2 extends BaseMonitorHubConnection2 {
     private static instance: NotificationsHubConnection2;
     
     private constructor(signalRBase, token) {
-        //const { sendNotification2 } = require('./SignalRConstants');
         super("/notification", signalRBase, token);
-        alert('test' + signalRBase + ' ' + token);
     }
 
     public static init(signalRBase, token) {
-        alert('init');
         if (!NotificationsHubConnection2.instance) {
             NotificationsHubConnection2.instance = new NotificationsHubConnection2(signalRBase, token);
         }
--- test.ts
+++ test.ts
@@ -1,6 +1,3 @@
-//import "./signalr/NotificationsHubConnection2";
-//import { NotificationsHubConnection2 } from "./signalr/NotificationsHubConnection2";
-
 export interface StoredNotification {
     id: number;
     userEmail: string;
@@ -14,26 +11,15 @@ export interface StoredNotification {
 function registerNotification(signalRBase, token) {
     const { NotificationsHubConnection2 } = require('./signalr/NotificationsHubConnection2');
 
-    alert('Initialise ' + signalRBase);
     console.log("Initialize StoredNotificationHubConnection");
     NotificationsHubConnection2.init(signalRBase, token);
     NotificationsHubConnection2.getInstance().start();
-    alert('instantiated and started');
 }
 
 function getStatus() {
     const { NotificationsHubConnection2 } = require('./signalr/NotificationsHubConnection2');
     alert('status ' + JSON.stringify(NotificationsHubConnection2.getInstance().connection.state));
     return NotificationsHubConnection2.getInstance().connection.state;
-}
-
-function onReceivedEvent () {
-    const { NotificationsHubConnection2 } = require('./signalr/NotificationsHubConnection2');
-    const { ko } = require('knockout');
-
-    NotificationsHubConnection2.OnReceiveNotification((storedNotif: StoredNotification) => {
-        ko.postbox.publish("NotificationReceived");
-    });
 }
 
 function getInstance() {
@@ -45,6 +31,5 @@ function getInstance() {
 module.exports = {
     registerNotification,
     getStatus,
-    onReceivedEvent,
     getInstance
 }
